7eeecb7f9170bbb87b04471e7080b860edaf39e3
yusei53 <yusei530922@gmail.com>
2023-08-13 18:41:33 +0900

    一意性の検証

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_002614) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_092842) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
