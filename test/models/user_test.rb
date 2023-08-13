49fa02b569ee1c9326d36e4e667cec64370dbf56
yusei53 <yusei530922@gmail.com>
2023-08-13 15:52:48 +0900

    有効性を検証

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "EX User", email: "user@example.com")
-end
-
-test "name should not be too long" do
-  @user.name = "a" * 51
-  assert_not `user.valid?
-end
-
-`test "email should not be too long" do
-    @user.email = "a" * 244 + "@example.com"
-    assert_not @user.valid?
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  # test "name should not be too long" do
+  #   @user.name = "a" * 51
+  #   assert_not @user.valid?
+  # end
+
+  # test "email should not be too long" do
+  #   @user.email = "a" * 244 + "@example.com"
+  #   assert_not @user.valid?
+  # end
 end
